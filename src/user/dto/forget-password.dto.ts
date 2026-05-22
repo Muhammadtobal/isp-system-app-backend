a39f96b0518e3473743de5d1a67e62cadf9cfd47
MuhammadIT_53 <twbalmhmd091@gmail.com>
2026-05-22 16:04:53 +0300

    done

--- src/user/dto/forget-password.dto.ts
+++ src/user/dto/forget-password.dto.ts
@@ -1,16 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class ForgotPasswordInput {
+  @ApiProperty({
+    example: 'org@gmail.com',
+  })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 }
 
 export class ResetPasswordInput {
+  @ApiProperty({
+    example: 'knfslkrjt34t35t35',
+  })
   @IsNotEmpty()
   @IsString()
   token: string;
 
+  @ApiProperty({
+    example: 'newPassword',
+  })
   @IsNotEmpty()
   @IsString()
   newPassword: string;
